c3299aedc9c52630bbcb9ce8035dc13b4a9ec8ef
i11usion <david_luzhiju@163.com>
2021-02-23 08:12:16 +0800

    Added load feature for the game

--- src/main/java/student/adventure/Character.java
+++ src/main/java/student/adventure/Character.java
@@ -1,18 +1,34 @@
 package student.adventure;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-
+/**
+ * The character to go on to the adventure.
+ */
 public class Character {
     private room currentRoom;
     private Layout layout;
     private List<Item> items;
     private int levelOfForce;
+    private int load;
+
 
     public Character(Layout setLayout) {
         layout = setLayout;
         currentRoom = setLayout.getRooms()[0];
         items = new ArrayList<>();
+        load = 80;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void setLoad(int load) {
+        this.load = load;
     }
 
     public int getLevelOfForce() {
@@ -20,7 +36,7 @@ public class Character {
             return 0;
         }
         int countLevelOfForce = 0;
-        for (Item item: items) {
+        for (Item item : items) {
             countLevelOfForce += item.getLevelOfForce();
         }
         return countLevelOfForce;
@@ -50,12 +66,17 @@ public class Character {
         this.layout = layout;
     }
 
+    /**
+     * Get the names of the items in the inventory.
+     *
+     * @return the list of all the items in the inventory.
+     */
     public List<String> getItemsString() {
         if (items == null || items.size() == 0) {
             return new ArrayList<>();
         }
         List<String> itemStrings = new ArrayList<String>();
-        for (Item item: items) {
+        for (Item item : items) {
             itemStrings.add(item.getItemName());
         }
         return itemStrings;
@@ -63,14 +84,14 @@ public class Character {
 
     @Override
     public String toString() {
-        String toString = currentRoom.getDescriptions() +
-                "\nFrom here, you can go: ";
-        for (Direction direction: currentRoom.getDirections()) {
+        String toString = currentRoom.getDescriptions()
+                + "\nFrom here, you can go: ";
+        for (Direction direction : currentRoom.getDirections()) {
             toString += direction.getDirectionName() + " ";
         }
         if (currentRoom.getItems().size() != 0) {
             toString += "\nItems visible: ";
-            for (Item item: currentRoom.getItems()) {
+            for (Item item : currentRoom.getItems()) {
                 toString += item.toString() + ", ";
             }
             toString = toString.substring(0, toString.length() - 2);
@@ -78,6 +99,12 @@ public class Character {
         return toString;
     }
 
+
+    /**
+     * Get the command options of the character at the point.
+     *
+     * @return the commands that the character can receive at the point.
+     */
     public Map<String, List<String>> getCommandOptions() {
         Map<String, List<String>> commandOptions =  new HashMap<>();
         commandOptions.put("examine", new ArrayList<>());
